aff2d009775c4a1489f8174b353b577692ebac00
FuSoraS <developer.FuSoraS@gmail.com>
2026-05-08 11:19:58 -0400

    fix: remove test

--- console/src/modules/plan-internet-gb.ts
+++ console/src/modules/plan-internet-gb.ts
@@ -3,7 +3,7 @@ import plan from "../data/plan.json" with { type: "json" }
 
 const defaultDateReset = plan.default.dateReset
 
-export const dateNow = Temporal.Now.plainDateISO().subtract({days: 1})
+export const dateNow = Temporal.Now.plainDateISO()
 
 function getCycleDay(dateT = dateNow, dateReset = defaultDateReset) {
   // day 8 of the month is the reset day for the plan
